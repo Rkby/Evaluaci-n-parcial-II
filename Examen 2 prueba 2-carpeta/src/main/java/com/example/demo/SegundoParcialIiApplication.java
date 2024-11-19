c90ab1a8d3f2760eba96bbd2dfc587e8e0d31214
Rkby <158528017+Rkby@users.noreply.github.com>
2024-11-18 20:27:44 -0700

    Add files via upload

---
+++ Examen 2 prueba 2-carpeta/src/main/java/com/example/demo/SegundoParcialIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoParcialIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoParcialIiApplication.class, args);
+	}
+
+}
